ad3cf46723ec10162190856b6ccc0deffcbd882d
P Kelly <pkkelly2020@gmail.com>
2023-03-18 10:03:56 -0600

    Slider added

--- js/script.js
+++ js/script.js
@@ -92,63 +92,63 @@ const displayMovieDetails = async () => {
 
   div.innerHTML = `
   <div class="details-top">
-          <div>
-          ${
-            movie.poster_path
-              ? `<img
-              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-              class="card-img-top"
-              alt="${movie.title}"
-            />`
-              : `<img
-            src="images/no-image.jpg"
-            class="card-img-top"
-            alt="${movie.title}"
-          />`
-          }
-          </div>
-          <div>
-            <h2>${movie.title}</h2>
-            <p>
-              <i class="fas fa-star text-primary"></i>
-              ${movie.vote_average.toFixed(1)}/ 10
-            </p>
-            <p class="text-muted">Release Date: ${movie.release_date}</p>
-            <p>${movie.overview}</p>
-            <h5>Genres</h5>
-            <ul class="list-group">
-              ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
-            </ul>
-            <a href="${
-              movie.homepage
-            }" target="_blank" class="btn">Visit Movie Homepage</a>
-          </div>
-        </div>
-        <div class="details-bottom">
-          <h2>Movie Info</h2>
-          <ul>
-            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
-              movie.budget
-            )}</li>
-            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
-              movie.revenue
-            )}</li>
-            <li><span class="text-secondary">Runtime:</span> ${
-              movie.runtime
-            } minutes</li>
-            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
-          </ul>
-          <h4>Production Companies</h4>
-          <div class="list-group">
-          ${movie.production_companies
-            .map(
-              company => `
-          <span>${company.name}</span>`
-            )
-            .join('')}
-          </div>
-        </div>`
-  // console.log(div)
+  <div>
+  ${
+    movie.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+      ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${movie.production_companies
+      .map(company => `<span>${company.name}</span>`)
+      .join(', ')}
+  </div>
+</div>
+  `
+
   document.querySelector('#movie-details').appendChild(div)
 }
 
@@ -242,6 +242,53 @@ const displayBackgroundImage = (type, backgroundPath) => {
   }
 }
 
+// display Slider movies
+const displaySlider = async () => {
+  const { results } = await fetchAPIData('movie/now_playing')
+
+  results.forEach(movie => {
+    const div = document.createElement('div')
+    div.classList.add('swiper-slide')
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+              <img src="https://tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+    `
+
+    document.querySelector('.swiper-wrapper').appendChild(div)
+
+    initSwiper()
+  })
+}
+
+// set swiper up with options
+const initSwiper = () => {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  })
+}
+
 // fetch data from TMDB API - we'll call this from multiple places in the app
 const fetchAPIData = async endpoint => {
   // don't do this in a production environment
@@ -282,6 +329,7 @@ function init() {
     case '/':
     case '/index.html':
       displayPopularMovies()
+      displaySlider()
       break
     case '/shows.html':
       displayPopularShows()
